c30764ee80ae98c6bbfa076d72e368b846242138
Avishek Amin <avishekamin207@gmail.com>
2026-07-04 12:41:42 +0530

    Added Leetcode-867: Transpose Matrix solution

---
+++ Arrays/TransposeMatrix867.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+// LeetCode 867: Transpose Matrix
+// Difficulty: Easy
+
+// Approach:
+// - Determine the number of rows and columns in the input matrix.
+// - Create a new matrix with dimensions swapped:
+//   (columns × rows).
+// - Traverse each element of the original matrix.
+// - Place matrix[i][j] at transpose[j][i].
+// - Return the transposed matrix.
+
+// Time Complexity: O(m × n)
+// Space Complexity: O(m × n)
+
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int transpose[][] = new int[col][row];
+
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+        return transpose;
+    }
+}
